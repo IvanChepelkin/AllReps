b357cda2c945655f20dc100ec67f806e75992e40
SpartakChampion <ivan_chepelkin@mail.ru>
2018-12-17 16:56:36 +0300

    Initial commit

---
+++ app/src/main/java/com/example/user/allreps/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.user.allreps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.arellomobile.mvp.MvpAppCompatActivity;
+
+public class MainActivity extends MvpAppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
